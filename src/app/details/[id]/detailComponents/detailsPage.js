6c4e6c48961f8c7b3ea4d4746fea2fdcaf1db14c
betawars@oregonstate.edu <betawars@oregonstate.edu>
2025-04-14 17:05:04 -0700

    commit-13: Initial details changes

--- src/app/details/[id]/detailComponents/detailsPage.js
+++ src/app/details/[id]/detailComponents/detailsPage.js
@@ -1,7 +1,19 @@
-export default function DetailsPage({movieData}){
-    console.log(movieData)
+import "./styles.css"
 
+export default function DetailsPage({movieData}){
     return (
-        <div></div>
+        <div className="details-main">
+            <div className="details-main-picture">
+                picture
+            </div>
+            <div className="details-main-info">
+                details
+            </div>
+            <div className="details-main-meta">
+                meta
+            </div>
+
+
+        </div>
     )
 }
