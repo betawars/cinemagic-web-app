7514bbb98f7e561263ed55a12a493224542c29de
betawars@oregonstate.edu <betawars@oregonstate.edu>
2025-04-15 20:16:36 -0700

    commit-14: Started with the details page

--- src/app/details/[id]/detailComponents/detailsPage.js
+++ src/app/details/[id]/detailComponents/detailsPage.js
@@ -1,18 +1,46 @@
+import Image from "next/image"
 import "./styles.css"
+import { Suspense } from "react"
+import LoadingSpinner from "@/components/loadingSpinner"
+import MoviePoster from "./moviePoster"
 
-export default function DetailsPage({movieData}){
+export default function DetailsPage({ movieData }) {
+
+    console.log(movieData)
     return (
         <div className="details-main">
-            <div className="details-main-picture">
-                picture
-            </div>
-            <div className="details-main-info">
-                details
-            </div>
-            <div className="details-main-meta">
-                meta
-            </div>
+            <img
+                src={`https://image.tmdb.org/t/p/w780${movieData.backdrop_path}`}
+                alt={movieData.title}
+                height={"100vh"}
+                width={"100%"}
+                className="details-background-image"
+            />
+            <div className="details-main-inner-container">
+                <div className="details-main-picture">
+                    <div className="mt-20">
+                        <MoviePoster
+                            src={`https://image.tmdb.org/t/p/w780${movieData.poster_path}`}
+                            alt={movieData.title}
+                        />
+                    </div>
+                </div>
+                <div className="details-main-info">
+                    <div className="mt-20">
+                        <h2 className="text-5xl">{movieData.title}</h2>
+                        <div className="mt-5">
+                            <button className="bg-[#c7b9d9] hover:bg-[#69a5ef] text-white font-sans text-[15px] px-5 py-2 rounded-[28px] border border-[#b9a7d7] shadow-sm hover:shadow-md active:relative active:top-px">Watch Now</button>
+                            <button className="ml-3 bg-[#ffffff] hover:bg-[#69a5ef] text-black font-sans text-[15px] px-5 py-2 rounded-[28px] border border-[#b9a7d7] shadow-sm hover:shadow-md active:relative active:top-px">Add to list</button></div>
 
+                        <p className="mt-5">{movieData.overview}</p>
+                    </div>
+                </div>
+                <div className="details-main-meta">
+                    <div className="details-main-meta-inner">
+                        
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
---
+++ src/app/details/[id]/detailComponents/moviePoster.js
@@ -0,0 +1,23 @@
+'use client'
+import { useState } from 'react'
+import Image from 'next/image'
+import LoadingSpinner from "@/components/loadingSpinner"
+
+export default function MoviePoster({ src, alt }) {
+  const [isLoading, setIsLoading] = useState(true)
+  
+  return (
+    <div className="relative">
+      {isLoading && <LoadingSpinner />}
+      <Image
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        src={src}
+        alt={alt}
+        height={384}
+        width={256}
+        style={{ objectFit: "contain" }}
+        onLoadingComplete={() => setIsLoading(false)}
+      />
+    </div>
+  )
+}
