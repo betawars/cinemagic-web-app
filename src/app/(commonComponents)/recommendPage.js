9801bc4e6fd108c1b6a7fff16267606cc68f2dbb
betawars@oregonstate.edu <betawars@oregonstate.edu>
2025-04-18 22:54:06 -0700

    commit-16: Added Recommended page, needs work

---
+++ src/app/(commonComponents)/recommendPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecommendPage = () => {
+    return (
+        <div className='recommend-main'>
+           
+        </div>
+    );
+};
+
+export default RecommendPage;
