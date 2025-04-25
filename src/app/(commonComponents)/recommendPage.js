ae1e8bcd50cc528f93e5cff048546a4674254591
betawars@oregonstate.edu <betawars@oregonstate.edu>
2025-04-24 21:43:35 -0700

    commit-18: Initiation of the recommended component

--- src/app/(commonComponents)/recommendPage.js
+++ src/app/(commonComponents)/recommendPage.js
@@ -3,7 +3,15 @@ import React from 'react';
 const RecommendPage = () => {
     return (
         <div className='recommend-main'>
-           
+            <h1>Recommend Page</h1>
+            <p>This is the recommend page.</p>
+            <div className='recommend-content'>
+                <p>Here you can find recommendations based on your preferences.</p>
+                <p>Explore new content and discover what you love!</p>
+            </div>
+            <div className='recommend-footer'>
+                <p>Footer content goes here.</p>
+            </div>
         </div>
     );
 };
